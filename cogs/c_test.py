bd8964bb0c4d85ad6fc8cfe44eca05215ea10571
Maxime Fournes <fournes.maxime@gmail.com>
2024-04-02 13:47:30 +0200

    Added display of user information in test command

--- cogs/c_test.py
+++ cogs/c_test.py
@@ -2,6 +2,13 @@ from discord.ext import commands
 from typing import Tuple
 from discord_tools import confirm_dialogue
 
+def get_user_info_str(context: commands.Context) -> str:
+    usr = context.author
+    return ( f"Display Name: {usr.display_name}\n" +
+             f"Name: {usr.name}\n" +
+             f"Global Name: {usr.global_name}\n" +
+             f"ID: {usr.id}" )
+
 class TestCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,6 +16,7 @@ class TestCog(commands.Cog):
 
     @commands.command(name="test", description="Test")
     async def test(self, context: commands.Context):
+        await context.send(get_user_info_str(context))
         create = await confirm_dialogue(self.bot, context, "Create Task?", "This is the task text `write` `research`", "blah")
         if create:
             await context.author.send("YEEES")
