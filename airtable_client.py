08ce652d8f41c7b452b70292501ed650d8a880a0
Maxime Fournes <fournes.maxime@gmail.com>
2024-04-07 17:21:50 +0200

    Improved airtable client

--- airtable_client.py
+++ airtable_client.py
@@ -1,26 +1,42 @@
 from airtable import Airtable
 import config
 
-projects_table = Airtable(config.airtable_base_id, config.projects_table_name, api_key=config.airtable_token)
-tasks_table = Airtable(config.airtable_base_id, config.tasks_table_name, api_key=config.airtable_token)
-projects_table_staging = Airtable(config.airtable_base_id_staging, config.projects_table_name, api_key=config.airtable_token)
-tasks_table_staging = Airtable(config.airtable_base_id_staging, config.tasks_table_name, api_key=config.airtable_token)
-onboarding_events_table = Airtable(config.airtable_base_id, config.onboarding_events_table_name, api_key=config.airtable_token)
-onboarding_events_table_staging = Airtable(config.airtable_base_id_staging, config.onboarding_events_table_name, api_key=config.airtable_token)
+TABLE_NAMES = [
+    "Projects",
+    "Tasks",
+    "Onboarding Events"
+]
 
 class Tables:
     def __init__(self):
         self.staging = False
+        self._tables = {}
+        self._tables_staging = {}
+        for table_name in TABLE_NAMES:
+            self._tables[table_name] = Airtable(
+                config.airtable_base_id, table_name, 
+                api_key=config.airtable_token)
+            self._tables_staging[table_name] = Airtable(
+                config.airtable_base_id_staging, table_name, 
+                api_key=config.airtable_token)
+            
     def set_staging(self):
         self.staging = True
+
+    def get_table(self, name: str) -> Airtable:
+        assert name in TABLE_NAMES, f"There is no table named {name}"
+        return self._tables_staging[name] if self.staging else self._tables[name]
+    
     @property
-    def projects(self):
-        return projects_table_staging if self.staging else projects_table
+    def projects(self) -> Airtable:
+        return self.get_table("Projects")
+    
     @property
-    def tasks(self):
-        return tasks_table_staging if self.staging else tasks_table
+    def tasks(self) -> Airtable:
+        return self.get_table("Tasks")
+    
     @property
-    def onboarding_events(self):
-        return onboarding_events_table_staging if self.staging else onboarding_events_table
+    def onboarding_events(self) -> Airtable:
+        return self.get_table("Onboarding Events")
 
 TABLES = Tables()
