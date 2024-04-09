906873f3bb268b946dc6bf001df0b5744ad89a57
Maxime Fournes <fournes.maxime@gmail.com>
2024-04-09 20:18:08 +0200

    Truncating how to help section from people joining on the website

--- polling_actions/p_new_member.py
+++ polling_actions/p_new_member.py
@@ -9,10 +9,14 @@ async def on_new_member(record_id: str):
     try:
         record = TABLES.join_pause_ai.get(record_id)
         onboarding_channel = client.get_channel(CONFIG.onboarding_channel_id)
+        how_to_help = record["fields"].get("How do you want to help?", "")
+        how_to_help_truncated = how_to_help[:300]
+        if len(how_to_help) > len(how_to_help_truncated):
+            how_to_help_truncated += "..."
         await onboarding_channel.send(NEW_SIGNUP.format(
             name=record["fields"].get("Name", "Anonymous"),
             country=record["fields"].get("Country", "?"),
-            how_to_help=record["fields"].get("How do you want to help?", ""),
+            how_to_help=how_to_help_truncated,
             record_id=record_id
         ))
     except Exception as e:
