bd65b0cc28a4a2b7ecc500af9e186c2d32a35024
Maxime Fournes <fournes.maxime@gmail.com>
2024-05-03 15:29:10 +0200

    modifying kudos to not display the action target name

--- messages/m_kudos.py
+++ messages/m_kudos.py
@@ -1,9 +1,9 @@
-KUDO_EMAIL = """🥳 Kudos to **{volunteer_name}** for sending an email to a `{target_type}`: {target_name} 🙌"""
-KUDO_MEETING = """🥳 Kudos to **{volunteer_name}** for meeting with a `{target_type}`: {target_name} 🙌"""
-KUDO_CALL = """🥳 Kudos to **{volunteer_name}** for calling a `{target_type}`: {target_name} 🙌"""
-KUDO_MESSAGE = """🥳 Kudos to **{volunteer_name}** for sending a message to a `{target_type}`: {target_name} 🙌"""
-KUDO_SNAILMAIL = """🥳 Kudos to **{volunteer_name}** for sending a snail-mail to a `{target_type}`: {target_name} 🙌"""
-KUDO_OTHER = """🥳 Kudos to **{volunteer_name}** for logging their work with {target_name} 🙌"""
+KUDO_EMAIL = """🥳 Kudos to **{volunteer_name}** for sending an email to a `{target_type}` 🙌"""
+KUDO_MEETING = """🥳 Kudos to **{volunteer_name}** for meeting with a `{target_type}` 🙌"""
+KUDO_CALL = """🥳 Kudos to **{volunteer_name}** for calling a `{target_type}` """
+KUDO_MESSAGE = """🥳 Kudos to **{volunteer_name}** for sending a message to a `{target_type}` 🙌"""
+KUDO_SNAILMAIL = """🥳 Kudos to **{volunteer_name}** for sending a snail-mail to a `{target_type}` 🙌"""
+KUDO_OTHER = """🥳 Kudos to **{volunteer_name}** for logging their work 🙌"""
 
 KUDO_TEMPLATES = {
     "email": KUDO_EMAIL,
