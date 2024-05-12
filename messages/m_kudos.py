c141490a8d86d487dba87b735397b41a01b309fe
Maxime Fournes <fournes.maxime@gmail.com>
2024-05-12 17:54:44 +0200

    updated kudos message to include a link to the outreach form

--- messages/m_kudos.py
+++ messages/m_kudos.py
@@ -1,9 +1,9 @@
-KUDO_EMAIL = """🥳 Kudos to **{volunteer_name}** for sending an email to a `{target_type}` 🙌"""
-KUDO_MEETING = """🥳 Kudos to **{volunteer_name}** for meeting with a `{target_type}` 🙌"""
-KUDO_CALL = """🥳 Kudos to **{volunteer_name}** for calling a `{target_type}` """
-KUDO_MESSAGE = """🥳 Kudos to **{volunteer_name}** for sending a message to a `{target_type}` 🙌"""
-KUDO_SNAILMAIL = """🥳 Kudos to **{volunteer_name}** for sending a snail-mail to a `{target_type}` 🙌"""
-KUDO_OTHER = """🥳 Kudos to **{volunteer_name}** for logging their work 🙌"""
+KUDO_EMAIL = """🥳 Kudos to **{volunteer_name}** for sending an email to a `{target_type}` 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
+KUDO_MEETING = """🥳 Kudos to **{volunteer_name}** for meeting with a `{target_type}` 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
+KUDO_CALL = """🥳 Kudos to **{volunteer_name}** for calling a `{target_type}` 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
+KUDO_MESSAGE = """🥳 Kudos to **{volunteer_name}** for sending a message to a `{target_type}` 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
+KUDO_SNAILMAIL = """🥳 Kudos to **{volunteer_name}** for sending a snail-mail to a `{target_type}` 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
+KUDO_OTHER = """🥳 Kudos to **{volunteer_name}** for logging their work 🙌 (submitted through the [outreach form](https://airtable.com/appWPTGqZmUcs3NWu/pagoraIwzlYm2QwjD/form))"""
 
 KUDO_TEMPLATES = {
     "email": KUDO_EMAIL,
